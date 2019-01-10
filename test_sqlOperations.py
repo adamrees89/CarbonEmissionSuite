d798062df3d7974484d9571dde56551e03f86011
Adam Rees <adamrees89@users.noreply.github.com>
2019-01-10 09:14:44 +0000

    Added test cases to SQL testing (#2)
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload

--- test_sqlOperations.py
+++ test_sqlOperations.py
@@ -6,15 +6,25 @@ Created on Mon Dec 17 16:54:47 2018
 """
 
 import unittest
-import sqlite3
 import os
 import DBEISCarbonFactors
 
 class test_InstanceNames(unittest.TestCase):
     
     def setUp(self):
-        conn = sqlite3.connect("mydatabase.db")
-        c = conn.cursor()
-        
+        self.year = 2018
+        pass
+
+    def test_create_sql_database(self):
+        try:
+            DBEISCarbonFactors.CarbonFactors.sqlCreateTable(
+                    self, "mydatabase.db")
+        except Exception as e:
+            self.fail(f"\nFailed to create SQLite3 database. Error: {e}")
+
     def tearDown(self):
         os.remove("mydatabase.db")
+
+
+if __name__ == '__main__':
+    unittest.main()
